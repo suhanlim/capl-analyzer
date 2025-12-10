eadde18d9f6a29624a6793f5e8a541e96aaea169
shlim <shlim@suresofttech.com>
2025-12-10 13:31:32 +0900

    feat: file read by command line param

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+use std::env;
+use std::fs;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+
+    let _file_path = &args[1];
+
+    println!("File {}",_file_path);
+
+    let contents = fs::read_to_string(_file_path)
+        .expect("read file");
+
+    println!("With text:\n{contents}")
 }
