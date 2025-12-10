92862cfb772f28e1aecfc88d41d9bdd5e691e961
suhanlim <soohan2009@naver.com>
2025-12-10 20:40:17 +0900

    refator: 커맨드 라인 파싱 로직 분리

--- src/main.rs
+++ src/main.rs
@@ -4,12 +4,15 @@ use std::fs;
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let _file_path = &args[1];
+    let _file_path = parse_config(&args);
 
-    println!("File {}",_file_path);
+    println!("File {}", _file_path);
 
-    let contents = fs::read_to_string(_file_path)
-        .expect("read file");
+    let contents = fs::read_to_string(_file_path).expect("read file");
 
     println!("With text:\n{contents}")
+}
+
+fn parse_config(args: &[String]) -> &str {
+    &args[1]
 }
